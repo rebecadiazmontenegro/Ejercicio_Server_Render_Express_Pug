6836598cbae9976829230882675c73127dee434c
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-11-12 17:41:57 +0100

    add/error404/morgan

--- routes/filmsWeb.route.js
+++ routes/filmsWeb.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const filmsWebController = require("../controllers/filmsWeb.controller");
 const router = express.Router();
+const error = require("../middlewares/error404");
 
 // Home
-router.get("/", filmsWebController.renderHome);
+router.get("/", error, filmsWebController.renderHome);
 
 // POST desde el formulario
-router.post("/film", filmsWebController.searchFilm);
+router.post("/film", error, filmsWebController.searchFilm);
 
 // GET por título
-router.get("/film/:title", filmsWebController.getFilm);
+router.get("/film/:title", error, filmsWebController.getFilm);
 
 module.exports = router;
